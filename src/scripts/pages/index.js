a785b763b78becfc0a04d97fe2c63293c47c2c79
Danil' Akhmetgaliev <akhm3tgal1ev@yandex.ru>
2022-10-28 19:20:19 +0300

    установить webpack

--- src/scripts/pages/index.js
+++ src/scripts/pages/index.js
@@ -1,4 +1,5 @@
 // подключение модулей
+import '../../pages/index.css';
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
 import PopupWithImage from '../components/PopupWithImage.js';
