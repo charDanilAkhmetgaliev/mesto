fb598c68b3fea99704c7bbf4a274bdf4fdb7d92c
Danil' Akhmetgaliev <akhm3tgal1ev@yandex.ru>
2022-10-28 15:15:27 +0300

    refactor: структурировать js файлы

---
+++ scripts/components/Popup.js
@@ -0,0 +1,32 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._closePopupButton = this._popup.querySelector('.popup__close-button');
+  }
+
+  _handleEscClose = (evt) => {
+    if (evt.key === "Escape" || evt.key === "Esc") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    document.addEventListener('keydown', this._handleEscClose);
+    this._closePopupButton.addEventListener('click', this.close.bind(this));
+    this._popup.addEventListener('click', (event) => {
+      if (event.target.classList.contains('popup')) {
+        this.close();
+      }
+    });
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    this.setEventListeners();
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+}
---
+++ scripts/components/Section.js
@@ -0,0 +1,22 @@
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._contrainer = document.querySelector(containerSelector);
+  }
+
+  addItem(element) {
+    this._contrainer.append(element);
+  }
+
+  _clear() {
+    this._contrainer.innerHTML = '';
+  }
+
+  setItems() {
+    this._clear();
+    this._items.forEach((item) => {
+      this._renderer(item);
+    });
+  }
+}
